ce5d4a0d44e338b8835efc7c9532c80d4c025ea1
Onur Akkaya <onur.akkaya@d-teknoloji.com.tr>
2019-11-12 22:44:23 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/yazilimguncelkonular/profilim/ProfilimFragment.java
@@ -0,0 +1,23 @@
+package com.example.yazilimguncelkonular.profilim;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yazilimguncelkonular.R;
+
+public class ProfilimFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View  view = inflater.inflate(R.layout.ders_listesi_fragment,container,false);
+
+        return view;
+    }
+}
+
---
+++ app/src/main/java/com/example/yazilimguncelkonular/profilim/ProfilimViewModel.java
@@ -0,0 +1,7 @@
+package com.example.yazilimguncelkonular.profilim;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ProfilimViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
